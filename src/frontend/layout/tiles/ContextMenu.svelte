080b7f060cace32a1324ff8c010b515f676bb2ca
CenturionLC <johnshepard357@gmail.com>
2023-05-20 18:54:42 +0200

    Implemented UI for right-clicking nodes

---
+++ src/frontend/layout/tiles/ContextMenu.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { Node } from "svelvet";
+
+  export let node: Node;
+  export let positionX: number;
+  export let positionY: number;
+  export let destroy: null | (() => void) = null;
+
+  let left = positionX + 20 + "px";
+  let top = positionY - 40 + "px";
+</script>
+
+<div id="Frame" style="left: {left};top:{top};">
+  <button class="Category">Close</button>
+  <button class="Category">Copy</button>
+  <button class="Category">Cut</button>
+  <button class="Category" on:click="{destroy}">Delete</button>
+</div>
+
+<style>
+  #Frame {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    width: 200px;
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.5);
+    border-radius: 10px;
+    padding: 10px;
+    z-index: 9999;
+  }
+
+  .Category:hover {
+    background-color: rgba(149, 148, 148, 0.435);
+  }
+</style>
--- src/frontend/layout/tiles/Graph.svelte
+++ src/frontend/layout/tiles/Graph.svelte
@@ -1,12 +1,77 @@
 <!-- The canvas which displays our beautiful Svelvet GUI graph -->
-<script>
-  import { Svelvet, ThemeToggle, Node, Background } from "svelvet";
+<script lang="ts">
+  import { Svelvet, ThemeToggle, Node } from "svelvet";
+
+  import ContextMenu from "./ContextMenu.svelte";
+
+  console.log("help me");
+
+  let ShowContext: Boolean;
+  let node: Node;
+  let x: number;
+  let y: number;
+
+  x = 0;
+  y = 0;
+
+  ShowContext = false;
+
+  window.addEventListener("click", handleClick);
+  window.addEventListener("contextmenu", handleClick);
+
+  function handleNewClick(event: CustomEvent) {
+    if (ShowContext) ShowContext = false;
+
+    node = event.detail.node;
+    x = event.detail.e.pageX;
+    y = event.detail.e.pageY;
+
+    if (event.detail.e.button == 0) {
+      // console.log("This is a left click!")
+      //To be implemented
+    } else if (event.detail.e.button == 2) {
+      // console.log("This is a right click!")
+      ShowContext = true;
+    }
+
+    // Prevent the event from reaching the original handler
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  function handleClick(event: MouseEvent) {
+    console.log(ShowContext);
+    if (ShowContext) ShowContext = false;
+
+    if (event.button === 0) {
+      // Left click
+    }
+  }
 </script>
 
 <Svelvet id="my-canvas" zoom="{0.5}" minimap theme="custom-theme">
-  <Node bgColor="#ec4899" height="{200}" position="{{ x: 100, y: 100 }}" />
+  <Node
+    bgColor="#ec4899"
+    height="{200}"
+    position="{{ x: 100, y: 100 }}"
+    on:nodeClicked="{handleNewClick}"
+  />
+
+  <Node
+    bgColor="#ec4899"
+    height="{200}"
+    position="{{ x: 200, y: 200 }}"
+    on:nodeClicked="{handleNewClick}"
+  />
+
   <ThemeToggle main="custom-dark" alt="light" slot="toggle" mainIcon="light_mode" />
 </Svelvet>
+
+{#if ShowContext}
+  <div>
+    <ContextMenu node="{node}" positionX="{x}" positionY="{y}" />
+  </div>
+{/if}
 
 <style>
   :root[svelvet-theme="custom-dark"] {
